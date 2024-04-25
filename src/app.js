bd518c527bbd70cf960144156ab5ca41b18d8480
Christiansgh <workmailchristian@gmail.com>
2024-04-25 22:15:33 +0200

    feat: Added logging & nodemon (hotreloading) w/ "npm start".

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const shoeRoutes = require('./api/routes/shoes');
+
+app.use(morgan('dev')); //logging
 
 app.use('/shoes', shoeRoutes);
 
