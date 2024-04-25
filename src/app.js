18e3c177fc330a15e6e8844b8bb09c1f72cd7420
Christiansgh <workmailchristian@gmail.com>
2024-04-25 23:41:10 +0200

    feat: Added basic 400s & 500s error handling.

--- src/app.js
+++ src/app.js
@@ -8,4 +8,21 @@ app.use(morgan('dev')); //logging
 
 app.use('/shoes', shoeRoutes);
 
+//request error handling (400s)
+app.use((req, res, next) => {
+    const error = new Error('Not found.');
+    error.status = 404;
+    next(error);
+});
+
+//program error handling (500s)
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
