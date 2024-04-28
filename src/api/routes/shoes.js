54ed30d055b5d04bcc9c71940fc8774744407c0d
Christiansgh <workmailchristian@gmail.com>
2024-04-28 23:59:28 +0200

    install: Added body-parser.

--- src/api/routes/shoes.js
+++ src/api/routes/shoes.js
@@ -8,8 +8,14 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-   res.status(201).json({
-        message: 'Posting a shoe!'
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
+
+    res.status(201).json({
+        message: 'Posting a shoe!',
+        createdProduct: product
     }); 
 });
 
--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const shoeRoutes = require('./api/routes/shoes');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.use(morgan('dev')); //logging
 
