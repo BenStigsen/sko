d5666e200aa1b6222f971ba74b9f96b191b873d8
Christiansgh <workmailchristian@gmail.com>
2024-04-30 01:06:34 +0200

    feat: Added functional methods that query the database correctly now, for all CRUD operations. TODO: link them to the RESTful API.

--- src/models/shoes.js
+++ src/models/shoes.js
@@ -1,90 +1,92 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = require('./connection');
 const connection = require('./connection');
 
 class Shoe extends Model { }
+const sequelize = connection;
 Shoe.init(
     {
         name: DataTypes.STRING,
         price: DataTypes.INTEGER,
     },
-    { sequelize, modelName: 'shoe' },
-); //for some reason only sequelize is allowed. No other variable name.
+    { sequelize, modelName: 'shoe' },//for some reason only sequelize is allowed. No other variable name.
+);
 
+/*
+ * Helper function used to open a connection to the database then run a query.
+ */
 async function query(query) {
     await connection.sync();
     query();
-    return
 }
 
-createShoe(
-    () => {
-        const nike = Shoe.build({
-            name: 'test',
-            price: 9999,
-        });
-        console.log(nike.toJSON());
-    });
-
-query(
-    async () => {
-        const nike = Shoe.build({
-            name: 'testttttttttttttttttttttttttttttttttttttttt',
-            price: 99999999999999999999999,
-        });
-        console.log(nike.toJSON());
-    }
-);
-
 function createShoe(shoe) {
-    async () => {
+    query(async () => {
         const createdShoe = await Shoe.create({
             name: shoe.name,
             price: shoe.price
         });
-        console.log(createdShoe.toJSON())
-    }
-    return
+        console.log(createdShoe.toJSON());
+    });
 }
 
-async function createShoes(...shoes) {
-    for (const shoe of shoes) {
+function createShoes(...shoes) {
+    for (let shoe of shoes) {
         createShoe(shoe);
     }
-    return
 }
 
-async function readShoe(id) {
-    return Shoe.find(id => id === _id);
-}
-
-async function readShoes() {
-    return Shoe.findAll();
-}
-
-async function updateShoe(id, shoe) {
-    return Shoe.update({
-        _id: shoe.id,
-        name: shoe.name,
-        price: shoe.price
-    },
-        { where: { _id: id } }
-    )
-}
-
-async function deleteShoe(id) {
-    return Shoe.destroy({
-        where: {
-            _id: id
+function readShoe(id) {
+    query(async () => {
+        const found = await Shoe.findOne({ where: { id: id } });
+        if (found === null) {
+            console.log('No shoe found matching the id.');
+        } else {
+            console.log(found.toJSON());
+            return found;
         }
-    })
+    });
 }
 
-async function deleteShoes(...ids) {
+async function readShoes(print) {
+    const shoes = await Shoe.findAll();
+    if (print) {
+        for (let shoe of shoes) {
+            console.log(shoe.toJSON());
+        }
+    }
+    return shoes
+}
+
+function updateShoe(id, shoe) {
+    query(async () => {
+        await Shoe.update({
+            name: shoe.name,
+            price: shoe.price
+        },
+            { where: { id: id } }
+        )
+        console.log('Updated to:')
+        readShoe(id);
+    });
+}
+
+//Not checking if id exists in the database. Doesn't present an error if given invalid id.
+function deleteShoe(id) {
+    query(async () => {
+        await Shoe.destroy({
+            where: {
+                id: id
+            }
+        });
+    });
+    console.log('id: ' + id + ' successfully deleted.')
+}
+
+//Same for this since it invokes the other.
+function deleteShoes(...ids) {
     for (const id of ids) {
         deleteShoe(id);
     }
-    return
 }
 
 module.exports = {
