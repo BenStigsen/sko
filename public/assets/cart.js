5deb173e6712a8e451f390597d7788a82b1a3753
Benjamin Stigsen <git@stigsen.dev>
2024-05-23 11:29:30 +0200

    added product image and count to cart

--- public/assets/cart.js
+++ public/assets/cart.js
@@ -2,17 +2,18 @@ function store(obj) {
     let products = localStorage.getItem("products");
     if (products == null) {
         products = {"items": []};
+    } else {
+        products = JSON.parse(products);
+        products.items.push(obj);
     }
 
-    products = JSON.parse(products);
-    products.items.push(obj);
     localStorage.setItem("products", JSON.stringify(products));
 }
 
 function load() {
     let products = localStorage.getItem("products");
     if (products == null) {
-        return;
+        return [];
     }
     
     return JSON.parse(products).items;
@@ -33,10 +34,43 @@ function reset() {
     localStorage.clear();
 }
 
+function update() {
+    let cart = document.getElementById("cart-dropdown");
+    cart.innerHTML = "";
+
+    let products = load();
+
+    let unique = [...new Set(products.map(JSON.stringify))].map(JSON.parse);
+    for (let product of products) {
+        if (unique.find(p => p.name == product.name).count == undefined) {
+            unique.find(p => p.name == product.name).count = 1;
+        } else {
+            unique.find(p => p.name == product.name).count += 1;
+        }
+
+    }
+
+    for (let product of unique) {
+        let title = document.createElement("p");
+        title.innerHTML = product.name;
+
+        let image = document.createElement("img");
+        image.src = product.image;
+
+        let count = document.createElement("p");
+        count.innerHTML = product.count;
+        cart.appendChild(title);
+        cart.appendChild(image);
+        cart.appendChild(count);
+    }
+}
+
 document.getElementById("add-to-cart").addEventListener("click", () => {
     let product = {};
     product.name = document.getElementById("product-title").innerHTML;
     product.price = document.getElementById("product-price").innerHTML;
     product.image = document.getElementById("product-image").src;
     store(product);
+    
+    update();
 })
