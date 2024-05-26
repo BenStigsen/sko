b8cc4dfaeaf1e4fbbafc1fafbfc1f5a2b0782c14
Benjamin Stigsen <git@stigsen.dev>
2024-05-26 15:46:38 +0200

    added better cart styling and fixed errors

--- public/assets/cart.js
+++ public/assets/cart.js
@@ -12,13 +12,21 @@ function storeMap(map) {
     localStorage.products = JSON.stringify([...map]);
 }
 
+function addProduct(obj) {
+    let products = loadProducts();
+    obj.amount = products.has(obj.name) ? products.get(obj.name).amount + 1 : 1;
+    products.set(obj.name, obj);
+    storeMap(products);
+}
+
 function storeProduct(obj) {
     let products = loadProducts();
+    if (obj.amount <= 0) {
+        removeProduct(obj.name);
+        return;
+    }
 
-    let product = products.get(obj.name) ?? obj;
-    product.amount = product.amount ? product.amount + 1 : 1;
-
-    products.set(product.name, product);
+    products.set(obj.name, obj);
     storeMap(products);
 }
 
@@ -50,33 +58,50 @@ function updateProducts() {
     for (let [key, product] of products) {
         totalPrice += Number(product.price) * product.amount;
 
-        let title = document.createElement("p");
-        title.innerHTML = key;
+        let div = document.createElement("div");
+        div.classList.add("relative");
 
         let image = document.createElement("img");
         image.src = product.image;
+        
+        let title = document.createElement("p");
+        title.innerHTML = key;
 
+        let price = document.createElement("small");
+        price.innerHTML = product.price + "kr";
+        
         let amount = document.createElement("input");
         amount.type = "number";
         amount.value = product.amount;
+        amount.classList.add("bg-gray-50", "border", "text-sm", "rounded-lg", "focus:ring-blue-500", "focus:border-blue-500", "block", "w-14", "h-6", "p-1");
         amount.setAttribute("data-target", product.name);
         amount.addEventListener("change", (event) => {
-            loadProduct(event.target.getAttribute("data-target")).amount = event.target.value;
+            let product = loadProduct(event.target.getAttribute("data-target"));
+            product.amount = event.target.value;
+            storeProduct(product);
             updateProducts();
         });
 
-        let remove = document.createElement("a");
-        remove.innerHTML = "Remove";
+        let remove = document.createElement("img");
+        remove.src = "https://www.svgrepo.com/show/489107/remove-circle.svg";
+        remove.classList.add("w-5", "h-5", "cursor-pointer", "hover:text-red-500", "hover:scale-110", "absolute", "top-1", "right-1");
         remove.setAttribute("data-target", product.name);
         remove.addEventListener("click", (event) => {
             removeProduct(event.target.getAttribute("data-target"));
             updateProducts();
         });
 
-        cart.appendChild(title);
-        cart.appendChild(image);
-        cart.appendChild(amount);
-        cart.appendChild(remove);
+        div.appendChild(remove);
+        div.appendChild(image);
+        div.appendChild(title);
+        div.appendChild(price);
+        div.appendChild(amount);
+        cart.appendChild(div);
+        cart.appendChild(document.createElement("br"));
+
+        let hr = document.createElement("hr");
+        hr.classList.add("mb-2");
+        cart.appendChild(hr);
     }
 
     let total = document.createElement("p");
@@ -89,7 +114,8 @@ document.getElementById("add-to-cart").addEventListener("click", () => {
     product.name = document.getElementById("product-title").innerHTML;
     product.price = document.getElementById("product-price").innerHTML;
     product.image = document.getElementById("product-image").src;
-    storeProduct(product);
+    product.amount = 1;
+    addProduct(product);
     
     updateProducts();
 })
