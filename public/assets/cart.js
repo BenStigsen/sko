f0478b60e7beb6382adecd23fc1883bfc6238087
Brozat <steje02@eamv.dk>
2024-05-27 11:17:18 +0200

    Added red dash to cart

--- public/assets/cart.js
+++ public/assets/cart.js
@@ -85,7 +85,7 @@ function updateProducts() {
         });
 
         let remove = document.createElement("img");
-        remove.src = "https://www.svgrepo.com/show/489107/remove-circle.svg";
+        remove.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngitem.com%2Fpimgs%2Fm%2F9-93082_transparent-stop-sign-png-does-red-circle-with.png&f=1&nofb=1&ipt=120ae7219d909e98c9749453e54c53e235b7211207a4c605fd3c044f48c7db71&ipo=images";
         remove.classList.add("w-5", "h-5", "cursor-pointer", "hover:text-red-500", "hover:scale-110", "absolute", "top-1", "right-1");
         remove.setAttribute("data-target", product.name);
         remove.addEventListener("click", (event) => {
