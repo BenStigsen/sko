21afc27bf07fe1439c38b9d33b595e7331a77401
Benjamin Stigsen <git@stigsen.dev>
2024-05-30 18:31:04 +0200

    jquery madness

--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -53,91 +53,65 @@ function updateProducts() {
     let totalPrice = 0;
     let products = loadProducts();
 
-    let container = document.createElement("div");
-    container.classList.add("flex", "flex-col");
+    let container = $("<div>", { class: "flex flex-col"});  
     for (let [key, product] of products) {
         totalPrice += Number(product.price) * product.amount;
 
-        let div = document.createElement("div");
-        div.classList.add("relative");
-
-        let image = document.createElement("img");
-        image.src = product.image;
-        image.setAttribute("alt", "Produkt " + product.name);
+        let div =   $("<div>",   { class: "relative" });
+        let image = $("<img>",   { src: product.image, alt: `Produkt ${product.name}` });
+        let title = $("<p>",     { html: `<b>${key}</b>` });
+        let price = $("<small>", { html: product.price + "kr" });
         
-        let title = document.createElement("p");
-        title.innerHTML = `<b>${key}</b>`;
+        let amount = $("<input>", {
+            value: product.amount,
+            type: "number",
+            class: "bg-gray-50 border-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-14 h-6 p-1",
+            title: `Amount of ${product.name}`,
+            "aria-label": `Amount of ${product.name}`,
+            "data-target": product.name
+        });
 
-        let price = document.createElement("small");
-        price.innerHTML = product.price + "kr";
-        
-        let amount = document.createElement("input");
-        amount.type = "number";
-        amount.value = product.amount;
-        amount.classList.add("bg-gray-50", "border-2", "text-sm", "rounded-lg", "focus:ring-blue-500", "focus:border-blue-500", "block", "w-14", "h-6", "p-1");
-        amount.setAttribute("data-target", product.name);
-        amount.setAttribute("aria-label", "Amount of " + product.name);
-        amount.setAttribute("title", "Amount of " + product.name);
-        amount.addEventListener("change", (event) => {
-            let product = loadProduct(event.target.getAttribute("data-target"));
+        amount.on("change", (event) => {
+            let product = loadProduct($(event.target).attr("data-target"));
             product.amount = event.target.value;
             storeProduct(product);
             updateProducts();
-        });
+        })
 
-        let remove = document.createElement("img");
-        remove.src = "/assets/img/remove.png";
-        remove.classList.add("w-5", "h-5", "max-sm:w-8", "max-sm:h-8", "cursor-pointer", "hover:text-red-500", "hover:scale-110", "absolute", "top-1", "right-1");
-        remove.setAttribute("data-target", product.name);
-        remove.setAttribute("aria-label", "Remove " + product.name);
-        remove.setAttribute("alt", "Remove " + product.name);
-        remove.addEventListener("click", (event) => {
-            removeProduct(event.target.getAttribute("data-target"));
+        let remove = $("<img>", {
+            src: "/assets/img/remove.png",
+            alt: `Remove ${product.name}`,
+            class: "w-5 h-5 max-sm:w-8 max-sm:h-8 cursor-pointer hover:text-red-500 hover:scale-110 absolute top-1 right-1",
+            "data-target": product.name,
+            "alt": `Remove ${product.name}`,
+        });
+        
+        remove.on("click", (event) => {
+            removeProduct($(event.target).attr("data-target"));
             updateProducts();
         });
 
-        div.appendChild(remove);
-        div.appendChild(image);
-        div.appendChild(title);
-        div.appendChild(price);
-        div.appendChild(amount);
-        container.appendChild(div);
-        container.appendChild(document.createElement("br"));
-        
-        let hr = document.createElement("hr");
-        hr.classList.add("mb-2");
-        container.appendChild(hr);
+        div.append([remove, image, title, price, amount]);
+        container.append([div, $("<br>"), $("<hr>", { class: "mb-2"})]);
     }
-
-    let cart = document.getElementById("cart-dropdown");
-    cart.innerHTML = "";
     
-    cart.appendChild(container);
-
-    let div = document.createElement("div");
-    div.classList.add("flex");
-
-    let total = document.createElement("p");
-    total.innerHTML = `<b>Total:</b> ${totalPrice} kr.`;
-    total.classList.add("justify-start");
-
-    let goToCheckout = document.createElement("a");
-    goToCheckout.innerHTML = "Gå til betaling";
-    goToCheckout.classList.add("underline", "hover:scale-105", "font-bold", "duration-50", "ml-auto", "cursor-pointer");
-
-    div.appendChild(total);
-    div.appendChild(goToCheckout);
-    cart.appendChild(div);
+    let div          = $("<div>", { class: "flex"});
+    let total        = $("<p>", { html: `<b>Total:</b> ${totalPrice} kr.`, class: "justify-start"});
+    let goToCheckout = $("<a>", { html: "Gå til betaling", class: "underline hover:scale-105 font-bold duration-50 ml-auto cursor-pointer"});
+    
+    div.append([total, goToCheckout]);
+    $("#cart-dropdown").html(container).append(div);
 }
 
-document.getElementById("add-to-cart")?.addEventListener("click", () => {
-    let product = {};
-    product.name = document.getElementById("product-title").innerText;
-    product.price = document.getElementById("product-price").innerText;
-    product.image = document.getElementById("product-image").src;
-    product.amount = 1;
+$("#add-to-cart")?.on("click", () => {
+    let product = {
+        name: $("#product-title").text(),
+        price: $("#product-price").text(),
+        image: $("#product-image").attr("src"),
+        amount: 1
+    };
+
     addProduct(product);
-    
     updateProducts();
 });
 
